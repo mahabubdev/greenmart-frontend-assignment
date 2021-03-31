b391e511eef80e87aa2ecb98e336005199eabaa5
Md Mahabub Alam <mahabub2000bd@gmail.com>
2021-04-01 00:43:20 +0600

    products fetch --ok

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Default from '../layouts/Default';
 
 const Home = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getProducts() {
+            await fetch(process.env.REACT_APP_API_SERVER + '/api/products', {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(p => p.json())
+            .then(d => {
+                console.log(d)
+                setProducts([...d])
+            })
+            .catch(err => console.log(err))
+        }
+
+        getProducts();
+    }, [])
+
     return (
         <Default>
-            Home Page
+            <p>
+                { products.length > 0 ? products.length : (
+                    <span style={{color: 'red'}}>Empty products</span>
+                ) }
+            </p>
         </Default>
     );
 };
